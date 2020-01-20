7fb0703b5f9db7f4f90e5620de1ff67df3be7a6f
Ollyver <ollyq786@gmail.com>
2020-01-20 18:23:19 +0530

    Custom CollectionView Layout

--- CustomCollectionViewLayout/CustomCollectionViewLayout.swift
+++ CustomCollectionViewLayout/CustomCollectionViewLayout.swift
@@ -19,7 +19,7 @@ extension CustomCollectionViewProtocol {
     }
 }
 class CustomCollectionViewLayout: UICollectionViewLayout {
-    fileprivate let numberOfColoum = 3
+    fileprivate let numberOfColumns = 3
     fileprivate let cellPadding : CGFloat = 6
     fileprivate let cellHeight : CGFloat = 150
     
@@ -39,55 +39,66 @@ class CustomCollectionViewLayout: UICollectionViewLayout {
     }
     override func prepare() {
         guard cache.isEmpty == true, let collectioView = collectionView else {return}
-        let coloumWidth = contentWidth / CGFloat(numberOfColoum)
+        let columnWidth = contentWidth / CGFloat(numberOfColumns)
         var xOffSet = [CGFloat]()
         
-        for coloumn in 0..<numberOfColoum {
-            if coloumn == 0 {
+        for column in 0..<numberOfColumns {
+            if column == 0 {
                 xOffSet.append(0)
             }
-            if coloumn == 1 {
-                xOffSet.append(2*coloumWidth)
+            if column == 1 {
+                xOffSet.append(2*columnWidth)
             }
-            if coloumn == 2 {
-                xOffSet.append(coloumWidth)
+            if column == 2 {
+                xOffSet.append(columnWidth)
             }
         }
-        var col = 0
+        var column = 0
         var yOffSet = [CGFloat] ()
         for item in 0..<collectioView.numberOfItems(inSection: 0) {
             let indexPath = IndexPath(item: item, section: 0)
-            for coloumn in 0..<numberOfColoum {
-                switch coloumn {
+            for column in 0..<numberOfColumns {
+                switch column {
                 case 0,1:
                     yOffSet.append(2*cellPadding)
                 case 2: yOffSet.append(cellPadding + cellHeight)
                 default:
                     break
                 }
-                let frame = CGRect(x: xOffSet[coloumn], y: yOffSet[coloumn], width: coloumWidth, height: coloumWidth)
+            }
+                let height = cellPadding * 2 + cellHeight
+                let frame = CGRect(x: xOffSet[column], y: yOffSet[column], width: columnWidth, height: columnWidth)
                 let insetFrame = frame.insetBy(dx: cellPadding, dy: cellPadding)
+                
+                //Creating attributres for the layout and caching them
                 let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
                 attributes.frame = insetFrame
                 cache.append(attributes)
-                contentHeight = max(collectioView.frame.height, frame.maxY)
-                let height = cellPadding*2 + cellHeight
-                yOffSet[coloumn] = yOffSet[coloumn] + 2*(height - cellPadding)
+                print ("frame.maxY", frame.maxY)
+                
+                //We increase the max height of the content as we get more items
+                contentHeight = max(collectioView.frame.height + 10, frame.maxY)
+                yOffSet[column] = yOffSet[column] + 2*(height - cellPadding)
                 let numberOfItem = delegate?.numberOfItemsInCollectionView()
-                if let numberOfItems = numberOfItem, indexPath.item == numberOfItems - 1 {
-                    switch coloumn {
+                if let numberOfItems = numberOfItem, indexPath.item == numberOfItems - 1
+                {
+                    //In case we get to the last cell, we check the column of the cell before
+                    //The last one, and based on that, we change the column
+                    print ("indexPath.item: \(indexPath.item), numberOfItems: \(numberOfItems)")
+                    print ("A")
+                    switch column {
                     case 0,1:
-                        col = 2
+                        column = 2
                     case 2:
-                        col = 0
+                        column = 0
                     default:
                         return
                     }
-                }else {
-                    col = col < (numberOfColoum - 1) ? (coloumn + 1) : 0
+                } else  {
+                    print ("B")
+                    column = column < (numberOfColumns - 1) ? (column + 1) : 0
                 }
             }
-        }
     }
     
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
